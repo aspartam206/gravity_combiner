f659d9f2b54ddb7e195d32a666ef1e021e3c7aa8
Mochammad Rizki <cyber.light52@gmail.com>
2020-04-04 09:45:42 -0400

    cron push 04-April-2020	09:45:41

---
+++ download_list.sh
@@ -0,0 +1,7 @@
+date
+
+# pull from github
+cd /root/gravity_housekeeper && /usr/bin/git pull
+
+# pull blocklist
+cd /root/gravity_housekeeper && /usr/bin/python3.6 /root/gravity_housekeeper/download.py
